a6c37d1f5d0be1a72f9f64ace50f21ff6cd78096
Vladislav Kiryukhin <vkiryukhin@yandex-team.ru>
2023-06-18 01:41:27 +0300

    Update supported platforms

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftPixelmatch",
+    platforms: [.iOS(.v13), .macOS(.v10_15)],
     products: [
         .library(
             name: "SwiftPixelmatch",
